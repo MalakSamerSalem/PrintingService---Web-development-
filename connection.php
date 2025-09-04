7d6c953987adf114fbd074c050fe0efb1052560d
MalakSamerSalem <malaksalm8@gmail.com>
2025-09-04 21:33:03 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,13 @@
 <?php 
 //to connect to database 
-$serverName = "localhost"; 
-$userName = "root";
-$password = ""; 
-$dbname = "PrintingService";
+$serverName = "192.168.0.100";  // Hostname given by DirectAdmin
+$userName   = "clearpr1_PrintingService"; // DB username
+$password   = "PZBEuxXjgw2mfa3QXhhY";     // DB password
+$dbname     = "clearpr1_PrintingService"; // DB name
 
 //create connection
 $conn = mysqli_connect($serverName, $userName, $password, $dbname); 
+
 //check connection 
 if (!$conn){
     die("Connection failed" . mysqli_connect_error());

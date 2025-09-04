23179f23acdf4d1b45fa62f4c2dd5defac00cfc5
MalakSamerSalem <malaksalm8@gmail.com>
2025-09-04 22:15:54 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -17,3 +17,24 @@ else {
     }
 
 ?>
+
+
+<?php 
+/*
+//to connect to database 
+$serverName = "localhost"; 
+$userName = "root";
+$password = ""; 
+$dbname = "PrintingService";
+
+//create connection
+$conn = mysqli_connect($serverName, $userName, $password, $dbname); 
+//check connection 
+if (!$conn){
+    die("Connection failed" . mysqli_connect_error());
+}
+else {
+       // echo "Connection successfully"; 
+    }
+*/
+?>
